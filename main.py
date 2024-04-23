20e9a5ebf716a8dfc390175ee4ead9abafeb74d8
GTatertots <root@DESKTOP-OVBT6VQ.localdomain>
2024-04-23 14:42:31 -0600

    started some queries

--- main.py
+++ main.py
@@ -13,6 +13,36 @@ def getdb():
     con.execute('PRAGMA foreign_keys = ON')
     return con
 
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.argument('pokemon1')
+@click.argument('pokemon2')
+@click.argument('pokemon3')
+@click.argument('pokemon4')
+@click.argument('pokemon5')
+@click.argument('pokemon6')
+def create_team(pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6):
+    with getdb() as con:
+        c = con.cursor()
+        c.execute("INSERT INTO team (poke1, poke2, poke3, poke4, poke5, poke6) VALUES (?,?,?,?,?,?)", (pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6))
+        con.commit()
+    print(f"--> created team: [{pokemon1},{pokemon2},{pokemon3},{pokemon4},{pokemon5},{[pokemon6}]")
+
+@click.argument('team_id')
+def type_coverage(team_id):
+    with getdb() as con:
+        c = con.cursor()
+        c.execute("",team_id) #TODO
+        coverage = c.fetchall()
+        print(f"team {team_id} has a coverage of {coverage} types")
+        con.commit()
+
+cli.add_command(create_team)
+cli.add_command(type_coverage)
+
 #EXAMPLE ON HOW TO DO CLI
 #@click.group()
 #def cli():
