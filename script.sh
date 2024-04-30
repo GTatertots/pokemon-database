c6fe623914caf475c3687ea2d91fe150d37e881e
Ryan Crandall <Ryan.Crandall@utahtech.edu>
2024-04-30 12:32:49 -0600

    minor changes so I can pull

--- script.sh
+++ script.sh
@@ -9,3 +9,5 @@ python3 main.py create-specific-pokemon 9 Bubble Tackle Bite Counter        #Bla
 python3 main.py create-specific-pokemon 19 Tackle Tackle Tackle Tackle      #Rattata
 
 python3 main.py create-team 1 1 2 2 3 3 #create team with 2 Venu, 2 Char, 2 Blas
+
+python3 main.py powerful-moves 3 9 #Venusaur and Blastoise
